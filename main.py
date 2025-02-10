6ac9176057e21aef42c68603038f45e55fdaa0df
lilykrie <lilykrie@woodland.on.ca>
2025-02-10 12:12:41 -0500

    second edits

--- main.py
+++ main.py
@@ -2,6 +2,31 @@ from PIL import Image
 from PIL import ImageFilter
 from PIL import ImageEnhance
 
+def change_type():
+    # Save the image in a different format
+    newName = input("type in the new name of the image (format: name.type)")
+    # only works if file type in name and in type are equal
+    newType = input("type in the new file type or type 'OPTIONS' to see available options").upper()
+    if newType == "OPTIONS":
+        print(options)
+        newType = input("Now type in the type you want to change your image to")
+    if newType not in options:
+        newType = input("This type is not available. Choose a different one.")
+    im.save(newName, newType)
+
+def merge_bands():
+    r, g, b = im.split()
+    merged = Image.merge("RGB", (b, g, r))
+    merged.show()
+    return merged
+    # ALL BLACK TO WHITE??
+
+def apply_filter():
+    filtered = im.filter(ImageFilter.DETAIL)
+    filtered.show()
+    return filtered
+    # OTHER FILTERS??
+
 name = "dog.jpg"
 im = Image.open(name)
 # from urllib.request import urlopen
@@ -17,41 +42,25 @@ im = Image.open(name)
 print("this is the file type, the size and the mode of your selected image")
 print(im.format, im.size, im.mode)
 while True:
-    inp = input("If you want to edit your image type 'edit', if you want to change your file type type 'type', if you're done editing type 'done'")
+    inp = input("If you want to edit your image type 'edit', if you want to save your file type 'save'")
     if inp == 'edit':
         edit = input("Do you want to ...?")
         if edit == "resize":
             width = input("What width should your image have?")
             length = input("What length should your image have?")
             im = im.resize((width, length))
+            im.show()
             # WIDTH AND LENGTH SWAPPPED?
         if edit == "rotate":
             deg = int(input("How many degrees do you want to rotate (counter-clockwise)?"))
             # CHECK FOR ERROR
             im = im.rotate(deg)
         if edit == "filter":
-            im = im.filter(ImageFilter.DETAIL)
-            # OTHER FILTERS??
-        if edit == "enhance":
-            enh = ImageEnhance.Contrast(im)
-            enh.enhance(1.3).show("30% more contrast")
-            # CHANGE IMAGE AND OTHER ENHANCEMENTS??
+            im = apply_filter()
         if edit == "color":
-            r, g, b = im.split()
-            im = Image.merge("RGB", (b, g, r))
+            im = merge_bands()
+            continue
     elif inp == 'type':
-        # Save the image in a different format
-        options = []  # !!
-        newName = input("type in the new name of the image (format: name.type)")
-        # only works if file type in name and in type are equal
-        newType = input("type in the new file type or type 'OPTIONS' to see available options").upper()
-        if newType == "OPTIONS":
-            print(options)
-            newType = input("Now type in the type you want to change your image to")
-        if newType not in options:
-            newType = input("This type is not available. Choose a different one.")
-        im.save(newName, newType)
-        continue
-    elif inp == 'done':
+        change_type()
         break
 im.show()
