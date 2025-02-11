13d669015a95da8c4d4e35e4fc39eb340fd8a721
Lilycavalier <lilykrie@woodland.on.ca>
2025-02-10 19:09:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,66 +1,75 @@
 from PIL import Image
 from PIL import ImageFilter
-from PIL import ImageEnhance
+from urllib.request import urlopen
 
-def change_type():
+def change_type(oldImage):
+    change = input("Do you want to change the file type?").upper()
     # Save the image in a different format
-    newName = input("type in the new name of the image (format: name.type)")
-    # only works if file type in name and in type are equal
-    newType = input("type in the new file type or type 'OPTIONS' to see available options").upper()
-    if newType == "OPTIONS":
-        print(options)
-        newType = input("Now type in the type you want to change your image to")
-    if newType not in options:
-        newType = input("This type is not available. Choose a different one.")
-    im.save(newName, newType)
+    if change == "YES":
+        newName = input("type in the new name of the image and the new type(format: filename.type)")
+    # only works if file type in name is spelled correct!!
+    elif change == "NO":
+        newName = input("Please type in the name you want the image to be saved as")
+    oldImage.save(newName)
 
-def merge_bands():
-    r, g, b = im.split()
+def rotate(oldImage):
+    deg = int(input("How many degrees do you want to rotate (counter-clockwise)?"))
+    # CHECK FOR ERROR
+    rotated = oldImage.rotate(deg)
+    rotated.show()
+    return rotated
+
+def resize(oldImage):
+    width = input("What width should your image have?")
+    length = input("What length should your image have?")
+    resized = oldImage.resize((width, length))
+    resized.show()
+    return resized
+    # WIDTH AND LENGTH SWAPPPED?
+
+def merge_bands(oldImage):
+    r, g, b = oldImage.split()
     merged = Image.merge("RGB", (b, g, r))
     merged.show()
     return merged
     # ALL BLACK TO WHITE??
 
-def apply_filter():
-    filtered = im.filter(ImageFilter.DETAIL)
+def apply_filter(oldImage):
+    filtered = oldImage.filter(ImageFilter.DETAIL)
     filtered.show()
     return filtered
     # OTHER FILTERS??
 
-name = "dog.jpg"
-im = Image.open(name)
-# from urllib.request import urlopen
-# kind = input("Do you have your picture saved locally or from the internet?")
-# if kind == "locally":
-#     name = input("type in the name of your file")
-#     im = Image.open(name)
-# elif kind == "internet":
-#     url = input("copy the url of your selected picture here:")
-#     im = Image.open(urlopen(url))
-# else:
-#     print("invalid input")
-print("this is the file type, the size and the mode of your selected image")
-print(im.format, im.size, im.mode)
 while True:
+    name = "dog.jpg"
+    im = Image.open(name)
+    # kind = input("Do you have your picture saved locally or from the internet?")
+    # if kind == "locally":
+    #     name = input("type in the name of your file")
+    #     im = Image.open(name)
+    # elif kind == "internet":
+    #     url = input("copy the url of your selected picture here:")
+    #     im = Image.open(urlopen(url))
+    # else:
+    #     print("invalid input")
+    print("this is the file type and the size of your selected image")
+    print(im.format, im.size)
     inp = input("If you want to edit your image type 'edit', if you want to save your file type 'save'")
     if inp == 'edit':
         edit = input("Do you want to ...?")
         if edit == "resize":
-            width = input("What width should your image have?")
-            length = input("What length should your image have?")
-            im = im.resize((width, length))
-            im.show()
-            # WIDTH AND LENGTH SWAPPPED?
+            im = resize(im)
+            continue
         if edit == "rotate":
-            deg = int(input("How many degrees do you want to rotate (counter-clockwise)?"))
-            # CHECK FOR ERROR
-            im = im.rotate(deg)
+            im = rotate(im)
+            continue
         if edit == "filter":
-            im = apply_filter()
+            im = apply_filter(im)
+            continue
         if edit == "color":
-            im = merge_bands()
+            im = merge_bands(im)
             continue
     elif inp == 'type':
-        change_type()
+        change_type(im)
         break
 im.show()
