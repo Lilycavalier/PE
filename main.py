2e86788d9b910ea18846adcbcaaddc922bfa0a6f
Lilycavaliers <lilykrie@woodland.on.ca>
2025-02-11 19:48:57 -0500

    final version

--- main.py
+++ main.py
@@ -3,97 +3,118 @@ from PIL import ImageFilter
 from urllib.request import urlopen
 
 def change_type(oldImage):
-    try:
-        change = input("Do you want to change the file type?").upper()
-        # Save the image in a different format
-        if change == "YES":
-            newName = input("type in the new name of the image and the new type(format: filename.type)")
-        # only works if file type in name is spelled correct!!
-        elif change == "NO":
-            newName = input("Please type in the name you want the image to be saved as")
-    except:
-        print("error")
-    oldImage.save(newName)
+    while True:
+        try:
+            change = input("Do you want to change the file type?").upper()
+            # Save the image in a different format
+            if change == "YES":
+                newName = input("type in the new name of the image and the new type(eg. filename.type)")
+            # only works if file type in name is spelled correct!!
+            elif change == "NO":
+                newName = input("Please type in the name you want the image to be saved as")
+            oldImage.save(newName)
+            print("your edited picture has been saved!")
+            break
+        except:
+            print("invalid input. make sure you used a supported file type")
+            continue
 
 def rotate(oldImage):
-    try:
-        deg = int(input("How many degrees do you want to rotate (counter-clockwise)?"))
-    except:
-        print("error. please type in an integer")
-    rotated = oldImage.rotate(deg)
-    rotated.show()
-    return rotated
+    while True:
+        try:
+            deg = int(input("How many degrees do you want to rotate (counter-clockwise)?"))
+            rotated = oldImage.rotate(deg)
+            rotated.show()
+            return rotated
+        except:
+            print("invalid input. please type in an integer")
+            continue
 
 def resize(oldImage):
-    try:
-        width = input("What width should your image have?")
-        heigth = input("What heigth should your image have?")
-    except:
-        print("error")
-    resized = oldImage.resize((width, heigth))
-    resized.show()
-    return resized
+    while True:
+        try:
+            width = int(input("What width should your image have?"))
+            heigth = int(input("What heigth should your image have?"))
+            resized = oldImage.resize((width, heigth))
+            resized.show()
+            return resized
+        except:
+            print("invalid input. please type in an integer")
+            continue
 
 def change_color(oldImage):
-    try:
-        x1 = int(input("Type in the value of the red band for the color you want to change"))
-        y1 = int(input("Type in the value of the green band for the color you want to change"))
-        z1 = int(input("Type in the value of the blue band for the color you want to change"))
-        x2 = int(input("Type in the value of the red band for the color you want to change it to"))
-        y2 = int(input(Type in the value of the green band for the color you want to change it to))
-        z2 = int(input(Type in the value of the blue band for the color you want to change it to))
-        pixels = oldImage.load() # create the pixel map
-        for i in range(oldImage.size[0]): # for every pixel:
-            for j in range(oldImage.size[1]):
-                if pixels[i,j] != (x1, y1, z1):
-                    # change to black if not red
-                    pixels[i,j] = (x2, y2 ,z2)
-        changed = oldImage
-        changed.show()
-        return changed
-    except:
-        print("error")
+    while True:
+        try:
+            x1 = int(input("Type in the value of the red band for the color you want to change"))
+            y1 = int(input("Type in the value of the green band for the color you want to change"))
+            z1 = int(input("Type in the value of the blue band for the color you want to change"))
+            x2 = int(input("Type in the value of the red band for the color you want to change it to"))
+            y2 = int(input("Type in the value of the green band for the color you want to change it to"))
+            z2 = int(input("Type in the value of the blue band for the color you want to change it to"))
+            pixels = oldImage.load() # create the pixel map
+            for i in range(oldImage.size[0]): # for every pixel:
+                for j in range(oldImage.size[1]):
+                    if pixels[i,j] == (x1, y1, z1):
+                        # change to black if not red
+                        pixels[i,j] = (x2, y2 ,z2)
+            changed = oldImage
+            changed.show()
+            return changed
+        except:
+            print("error. please make sure you typed in everything correctly")
+            continue
     
 
 def apply_filter(oldImage):
-    try:
-        filtered = oldImage.filter(ImageFilter.DETAIL)
-        filtered.show()
-        return filtered
-    # OTHER FILTERS??
+    while True:
+        try:
+            filter = input("What filter do you want to apply? (detail, sharpen, blur, contour or smooth)")
+            if filter == "detail":
+                filtered = oldImage.filter(ImageFilter.DETAIL)
+            elif filter == "sharpen":
+                filtered = oldImage.filter(ImageFilter.SHARPEN)
+            elif filter == "blur":
+                filtered = oldImage.filter(ImageFilter.BLUR)
+            elif filter == "smooth":
+                filtered = oldImage.filter(ImageFilter.SMOOTH)
+            elif filter == "contour":
+                filtered = oldImage.filter(ImageFilter.CONTOUR)
+            filtered.show()
+            return filtered
+        except:
+            print("invalid input. make sure your spelling is correct")
+            continue
 
-# GO BACK IF ERRORS?? WHERE DOES EXCEPT GO?? AVOIR ERRORS??
+
+kind = input("Do you have your picture saved locally or from the internet?")
+if kind == "locally":
+    name = input("type in the name of your file (eg. filename.type")
+    im = Image.open(name)
+elif kind == "internet":
+    url = input("copy the url of your selected picture here:")
+    im = Image.open(urlopen(url))
+else:
+    print("invalid input")
+print("this is the file type and the size of your selected image")
+print(im.format, im.size)
 
 while True:
-    name = "dog.jpg"
-    im = Image.open(name)
-    # kind = input("Do you have your picture saved locally or from the internet?")
-    # if kind == "locally":
-    #     name = input("type in the name of your file")
-    #     im = Image.open(name)
-    # elif kind == "internet":
-    #     url = input("copy the url of your selected picture here:")
-    #     im = Image.open(urlopen(url))
-    # else:
-    #     print("invalid input")
-    print("this is the file type and the size of your selected image")
-    print(im.format, im.size)
     inp = input("If you want to edit your image type 'edit', if you want to save your file type 'save'")
     if inp == 'edit':
-        edit = input("Do you want to ...?")
+        edit = input("Do you want to 'rotate', 'resize', 'apply filter' or 'change color'?")
         if edit == "resize":
             im = resize(im)
             continue
         if edit == "rotate":
             im = rotate(im)
             continue
-        if edit == "filter":
+        if edit == "apply filter":
             im = apply_filter(im)
             continue
-        if edit == "color":
+        if edit == "change color":
             im = change_color(im)
             continue
-    elif inp == 'type':
+    elif inp == 'save':
         change_type(im)
         break
 im.show()
